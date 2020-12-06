e60b32722fc1d4309efd99fa0c900511bef3dc9c
Vsevolod Pavlovskyi <pavlovskyi.ve@gmail.com>
2020-12-06 14:05:24 +0200

    Initial Commit

---
+++ Homework/HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeworkApp.swift
+//  Homework
+//
+//  Created by Vsevolod Pavlovskyi on 06.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
